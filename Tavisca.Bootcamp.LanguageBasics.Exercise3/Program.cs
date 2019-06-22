8d0856a64a4d30c10b053f996e6e9f5e22e0873a
Chaman Chowdhary <cchowdhary@tavisca.in>
2019-06-22 23:32:29 +0530

    Program Done

--- Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
@@ -41,7 +42,93 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
         public static int[] SelectMeals(int[] protein, int[] carbs, int[] fat, string[] dietPlans)
         {
             // Add your code here.
-            throw new NotImplementedException();
+            int total = protein.Length;
+            List <int> copy = new List <int> ();
+            int [] meal = new int [dietPlans.Length];
+            int [] cal = new int [total];
+            int i,j;
+            for(i=0;i<total;i++)
+                cal[i] = 5*protein[i] + 5*carbs[i] + 9*fat[i];
+            
+            for(i=0;i<dietPlans.Length;i++)
+            {
+                copy.Clear();
+                for(j=0;j<total;j++)
+                    copy.Add(j);
+                int dietlen=dietPlans[i].Length;
+                foreach(var character in dietPlans[i])
+                {
+                    if(character=='c')
+                        meal[i]=index(character,carbs,ref copy,dietlen);
+                    else if(character=='C')
+                        meal[i]=index(character,carbs,ref copy,dietlen);
+                    else if(character=='p')
+                        meal[i]=index(character,protein,ref copy,dietlen);
+                    else if(character=='P')
+                        meal[i]=index(character,protein,ref copy,dietlen);
+                    else if(character=='t')
+                        meal[i]=index(character,cal,ref copy,dietlen);
+                    else if(character=='T')
+                        meal[i]=index(character,cal,ref copy,dietlen);
+                    else if(character=='f')
+                        meal[i]=index(character,fat,ref copy,dietlen);
+                    else if(character=='F')
+                        meal[i]=index(character,fat,ref copy,dietlen);
+                    else 
+                        meal[i]=0;
+                    if(meal[i] != -1)
+                        break;
+                    dietlen -=1;
+                }
+            }
+            return meal;
+            //throw new NotImplementedException();
+        }
+
+        private static int index(char letter,int []ingredient, ref List<int> copy,int len)
+        {
+            List<int> temp = new List<int>();
+            int index=0,max,min;
+            if(char.IsUpper(letter))
+            {
+                max = int.MinValue;
+                foreach(var x in copy)
+                {
+                    if(ingredient[x]>max)
+                    {
+                        max = ingredient[x];
+                        index = x;
+                    }
+                }
+
+                foreach(var y in copy)
+                {
+                    if(ingredient[y] == max)
+                        temp.Add(y);
+                }
+                copy = temp;
+            }
+            else
+            {
+                min = int.MaxValue;
+                foreach(var x in copy)
+                {
+                    if(ingredient[x]<min)
+                    {
+                        min = ingredient[x];
+                        index = x;
+                    }
+                }
+                foreach(var y in copy)
+                {
+                    if(ingredient[y] == min)
+                        temp.Add(y);
+                }
+                copy=temp;
+            }
+            if(copy.Count>1 && len != 1)
+                return -1;
+            return index;
         }
     }
 }
